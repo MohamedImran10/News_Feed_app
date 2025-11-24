5d205b35ee62813a53a002deab9e75ad185a34e1
Mohamed Imran M <mohamedimran1040@gmail.com>
2025-11-24 15:22:01 +0530

    bug fixed

--- RssReader/RssReader/settings.py
+++ RssReader/RssReader/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-=vavbx-uh*b^x9g==*ffv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,.onrender.com').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,https://news-feed-app-nffy.onrender.com').split(',')
 
 
 # Application definition
